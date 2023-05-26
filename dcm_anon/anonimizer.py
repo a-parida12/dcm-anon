88123ecd594c86363a81bcd9f90d894a94d1ec97
Abinav R <31827335+AbinavRavi@users.noreply.github.com>
2023-05-26 13:12:51 +0200

    build: rename package name and pyproject toml
    * build: rename package name and pyproject toml
    
    * docs: modified name in license

--- dcm_anon/anonimizer.py
+++ dcm_anon/anonimizer.py
@@ -6,13 +6,14 @@ import pydicom
 from utils.dcm import check_valid_dcm
 import glob
 
+
 class dsAnonimizer(Anonimizer):
-    def __init__(self, deep_anon:bool=False, remove_pvt_tags:bool=True):
+    def __init__(self, deep_anon: bool = False, remove_pvt_tags: bool = True):
         super().__init__(deep_anon)
         self.remove_pvt_tags = remove_pvt_tags
 
-    def anonimize(self, dataset:FileDataset)->FileDataset:
-        """ anonimise a pydicom dataset
+    def anonimize(self, dataset: FileDataset) -> FileDataset:
+        """anonimise a pydicom dataset
 
         Args:
             dataset (FileDataset): dataset to be anonimised
@@ -29,34 +30,31 @@ class dsAnonimizer(Anonimizer):
             dataset[elem.tag] = elem
 
         return dataset
-    
+
+
 class fileAnonimizer(dsAnonimizer):
-    def __init__(self, deep_anon:bool=False, remove_pvt_tags:bool=True):
+    def __init__(self, deep_anon: bool = False, remove_pvt_tags: bool = True):
         super().__init__(deep_anon, remove_pvt_tags)
 
-    def anonimize_file(self, input:str, output:str):
+    def anonimize_file(self, input: str, output: str):
         dataset = pydicom.dcmread(input, force=True)
-        dataset =  self.anonimise(dataset)
+        dataset = self.anonimise(dataset)
         dataset.save_as(output)
- 
-    def run(self, input:str, output:str)->None:
+
+    def run(self, input: str, output: str) -> None:
         self.anonimize_file(input, output)
-        
+
 
 class folderAnonimizer(fileAnonimizer):
-    def __init__(self, deep_anon:bool=False, remove_pvt_tags:bool=True):
+    def __init__(self, deep_anon: bool = False, remove_pvt_tags: bool = True):
         super().__init__(deep_anon, remove_pvt_tags)
- 
-    def run(self, input:str, output:str)->None:
-        files = sorted(glob.glob(os.path.join(input+'*')))
+
+    def run(self, input: str, output: str) -> None:
+        files = sorted(glob.glob(os.path.join(input + "*")))
         dcm_files = [file for file in files if check_valid_dcm(file)]
 
         print(f"Found {len(files)} in given folder out of which {len(dcm_files)} are DCMs")
         print("Anonimising all DCMs")
         for i, dcm in enumerate(tqdm.tqdm(dcm_files)):
-            out_file_path = os.path.join(output, str(i)+'.dcm')
+            out_file_path = os.path.join(output, str(i) + ".dcm")
             self.anonimize_file(dcm, out_file_path)
-
-
-
-    
