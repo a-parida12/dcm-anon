4ea58eea03dabee0eca4ecae0c15b98ce96e8551
Abinav R <31827335+AbinavRavi@users.noreply.github.com>
2023-05-29 20:14:05 +0200

    test: added tests for utils
    * build: rename package name and pyproject toml
    
    * docs: modified name in license
    
    * 11 modify the module name from dcm anon to dcm anon (#17)
    
    * fix(modulename): update to dcm_anon
    
    * added a dummy test to ci pipeline
    
    * test: Added stubs for missing tests
    
    * refactor: Modified date and time from datetime more library based than using string manipulation
    
    * test: added tests for datetime after modifying datetime but tests dont run on local
    
    * test: added type annotation
    
    * build: added dicom_Csv and corrected imports tests now run
    
    * test: added tests for hash and datetime

--- dcm_anon/anonimizer.py
+++ dcm_anon/anonimizer.py
@@ -3,7 +3,7 @@ from pydicom.dataset import FileDataset
 import tqdm
 import os
 import pydicom
-from utils.dcm import check_valid_dcm
+from dcm_anon.utils.dcm import check_valid_dcm
 import glob
 
 
--- tests/test_00_utils.py
+++ tests/test_00_utils.py
@@ -1,9 +1,56 @@
-# import pytest
+import pytest
+from dcm_anon.utils.datetime import get_datetime, get_date, get_time
+from dcm_anon.utils.hash import encrypt_string, get_digits, get_non_digits
 
-# from hapy_pypi import bool_val
 
-# @pytest.mark.base
-# def test_00_1_base_test():
-#     assert type(bool_val) == bool
-#     assert bool_val == True
+@pytest.mark.base
+def test_dummy():
+    # remove during dev
+    bool_val = True
+    assert type(bool_val) == bool
+    assert bool_val
 
+
+@pytest.mark.utils
+def test_get_datetime() -> None:
+    dt_string = get_datetime()
+    assert type(dt_string) == str
+    assert len(dt_string) == 14
+
+
+@pytest.mark.utils
+def test_get_date() -> None:
+    date = get_date()
+    assert type(date) == str
+    assert len(date) == 8
+
+
+@pytest.mark.utils
+def test_get_time() -> None:
+    time = get_time()
+    assert type(time) == str
+    assert len(time) == 6
+
+
+@pytest.mark.utils
+def test_encrypt_string() -> None:
+    test_string = "test_string"
+    hash = encrypt_string(test_string)
+    hex_value = "4b641e9a923d1ea57e18fe41dcb543e2c4005c41ff210864a710b0fbb2654c11"
+    assert hash == hex_value
+
+
+@pytest.mark.utils
+def test_get_digits() -> None:
+    test_string = "1a2b3c"
+    op = get_digits(test_string)
+    assert op == "123"
+    assert type(op) == str
+
+
+@pytest.mark.utils
+def test_get_non_digits() -> None:
+    test_string = "1a2b3c"
+    op = get_non_digits(test_string)
+    assert op == "abc"
+    assert type(op) == str
